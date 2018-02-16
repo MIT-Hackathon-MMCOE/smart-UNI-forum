19e091a4f4079d7c02b527f830d7e523cfd9c5c2
Omkar Pathak <omkarpathak27@gmail.com>
2018-02-17 03:31:02 +0530

    To make even

--- smart_UNI_forum/smart_UNI_forum/urls.py
+++ smart_UNI_forum/smart_UNI_forum/urls.py
@@ -15,22 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-
-# class UserSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = User
-# 		fields = ('username', 'email')
-
-# # ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-# 	queryset = User.objects.all()
-# 	serializer_class = UserSerializer
-
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
